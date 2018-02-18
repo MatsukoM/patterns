116594ea5c7c19ee3cc446687125ce9557b7ac5f
Isaac Newtoon <couagedes@gmail.com>
2018-02-18 15:51:49 +0800

    Initial Project Version

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pushd ~/projects/java/design_patterns_tutorial/ > /dev/null
+
+# javac -d bin -sourcepath src src/com/patterns/helloworld/Test.java
+# java -classpath bin com.patterns.helloworld.Test
+
+javac -d bin -sourcepath src src/com/patterns/intro/Animal.java
+javac -d bin -sourcepath src src/com/patterns/intro/Dog.java
+javac -d bin -sourcepath src src/com/patterns/intro/Cat.java
+javac -d bin -sourcepath src src/com/patterns/intro/WorkWithAnimals.java
+
+javac -d bin -sourcepath src src/com/patterns/intro/Creature.java
+javac -d bin -sourcepath src src/com/patterns/intro/Giraffe.java
+javac -d bin -sourcepath src src/com/patterns/intro/Living.java
+javac -d bin -sourcepath src src/com/patterns/intro/Monkey.java
+
+
+java -classpath bin com.patterns.intro.WorkWithAnimals
+
+popd > /dev/null
+
